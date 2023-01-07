622e190a4b8ce2ef8a0714577e51c3b4aeba5f77
xiaobaicainiao666 <xiaobaicainiao@163.com>
2023-01-07 22:40:31 +0800

    update vignettes

--- R/RunGSEA_plant.R
+++ R/RunGSEA_plant.R
@@ -1,6 +1,6 @@
 
 #' Perform Gene Set Enrichment Analysis (GSEA) on Seurat object
-#' 
+#'
 #' Perform Gene Set Enrichment Analysis (GSEA) on Seurat object. GSEA is implemented using \code{clusterProfiler} package.
 #' This function is only for three plant species: \code{Arabidopsis thaliana} or \code{Oryza sativa} or \code{Zea mays}.
 #'
@@ -8,7 +8,8 @@
 #' @param by GO KEGG. Will be ignored if parameter "TERM2GENE" is not NULL.
 #' @param TERM2GENE Customized terms. Annotation of TERM TO GENE mapping, a data.frame of 2 column with term and gene. NULL by default. If not NULL, parameter "by" will be ignored.
 #' @param GeneIDtype Gene ID type in Seurat object. \code{TAIR} for \code{Arabidopsis thaliana}, for example.
-#' 
+#' @param minpct minimum expression percent in each cluster, parameter used to filter Differential expression genes to run GSEA. 0 as default.
+#'
 #' @importFrom clusterProfiler GSEA gseGO gseKEGG bitr
 #' @importFrom magrittr `%>%`
 #' @importFrom foreach foreach `%do%`
@@ -19,25 +20,26 @@
 #' @examples
 #' \dontrun{
 #' SeuratObj <- RunGSEA_plant(SeuratObj, by = 'GO', GeneIDtype = 'TAIR')
-#' } 
-#' 
+#' }
+#'
 RunGSEA_plant <- function(SeuratObj,
                           by = 'GO',
                           TERM2GENE = NULL,
-                          GeneIDtype = 'TAIR') {
-  
+                          GeneIDtype = 'TAIR',
+                          minpct = 0) {
+
   by <- match.arg(by, choices = c("GO", "KEGG"))
-  
+
   if (!"Allmarkers" %in% names(slot(object = SeuratObj, name = 'misc'))) {
     stop("Please run 'RunSeurat()' or 'Seurat::FindAllMarkers()', differentially expressed genes at
          SeuratObj@misc$Allmarkers are required to perform GSEA.")
   }
   Allmarkers <- slot(object = SeuratObj, name = 'misc')[["Allmarkers"]]
   Allmarkers$cluster <- as.character(Allmarkers$cluster)
-  
+
   if (!is.null(TERM2GENE)) {
     out <- foreach(cls=unique(Allmarkers$cluster)) %do% {
-      submarkers <- Allmarkers %>% dplyr::filter(cluster==cls)
+      submarkers <- Allmarkers %>% dplyr::filter(cluster==cls & pct.1 >= minpct)
       geneList <- sort(setNames(submarkers$avg_log2FC, submarkers$gene), decreasing = T)
       res <- GSEA(geneList     = geneList,
                   TERM2GENE    = TERM2GENE,
@@ -47,19 +49,19 @@ RunGSEA_plant <- function(SeuratObj,
       df
     }
   } else {
-    
+
     if (!"species" %in% names(slot(object = SeuratObj, name = 'misc'))) {
       stop("Please run 'readData()' and specify the 'species' parameter.")
     }
     species <- slot(object = SeuratObj, name = 'misc')[["species"]]
-    
+
     if (is.null(GeneIDtype)) {
       if (!"featureData" %in% names(slot(object = SeuratObj, name = 'misc'))) {
         stop("Please specify the 'GeneIDtype' parameter or run 'DetectGeneIDtype()' to determine the gene ID type of your data.")
       }
       GeneIDtype <- slot(object = SeuratObj, name = 'misc')[["featureData"]][['GeneIDtype']]
     }
-    
+
     if (by == 'GO') {
       # load the OrgDb
       species <- switch(species,
@@ -67,9 +69,9 @@ RunGSEA_plant <- function(SeuratObj,
                         'Oryza sativa' = 'Osa',
                         'Zea mays' = 'Zma')
       orgdb <- get_orgDb(species)
-      
+
       out <- foreach(cls=unique(Allmarkers$cluster)) %do% {
-        submarkers <- Allmarkers %>% dplyr::filter(cluster==cls)
+        submarkers <- Allmarkers %>% dplyr::filter(cluster==cls & pct.1 >= minpct)
         geneList <- sort(setNames(submarkers$avg_log2FC, submarkers$gene), decreasing = T)
         res <- gseGO(geneList     = geneList,
                      OrgDb        = orgdb,
@@ -84,7 +86,7 @@ RunGSEA_plant <- function(SeuratObj,
         df
       }
     }
-    
+
     if (by == 'KEGG') {
       kegg_code <- CellFunTopic:::get_kegg_code(species)
       # gene ID transition
@@ -97,9 +99,9 @@ RunGSEA_plant <- function(SeuratObj,
         bitrDF <- bitr(Allmarkers$gene, fromType = GeneIDtype, toType = "ENTREZID", OrgDb = orgdb, drop = TRUE)
         Allmarkers <- merge(Allmarkers, bitrDF, by.x = "gene", by.y = GeneIDtype)
       }
-      
+
       out <- foreach(cls=unique(Allmarkers$cluster)) %do% {
-        submarkers <- Allmarkers %>% dplyr::filter(cluster==cls)
+        submarkers <- Allmarkers %>% dplyr::filter(cluster==cls & pct.1 >= minpct)
         geneList <- sort(setNames(submarkers$avg_log2FC, submarkers$ENTREZID), decreasing = T)
         res <- gseKEGG(geneList          = geneList,
                        organism          = kegg_code,
@@ -116,14 +118,14 @@ RunGSEA_plant <- function(SeuratObj,
       }
     }
   }
-  
+
   result <- do.call("rbind", out)
   if (!is.null(TERM2GENE)) {
     slot(object = SeuratObj, name = 'misc')[["GSEAresult_customized"]] <- result
   } else {
     slot(object = SeuratObj, name = 'misc')[[paste0("GSEAresult_", by)]] <- result
   }
-  
+
   return(SeuratObj)
 }
 
